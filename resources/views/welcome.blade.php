6bd9b5bc291ae9c06c341e32aa34a71b76080bc4
rifqisbgj <rifqi.subagja4@gmail.com>
2021-10-07 15:56:15 +0700

    change title content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hubin SMKN 13 Bandung
                 </div>
 
                 <div class="links">

db6c454bae7cd817eaebc4565fb4690acdb07be7
sglkc <31957516+sglkc@users.noreply.github.com>
2021-10-11 16:07:51 +0700

    Revert commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,11 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rule;
 use App\Admin;
-use App\Siswa;
 
 class AdminController extends Controller
 {
@@ -50,43 +47,5 @@ class AdminController extends Controller
         }
 
         return redirect()->back()->withInput($credentials);
-    }
-
-    public function dataSiswa()
-    {
-        $datasiswa = Siswa::toBase()->get();
-        return view('admin.siswa', compact('datasiswa'));
-    }
-
-    public function tambahSiswa(Request $request)
-    {
-        $tahun = date('Y');
-        $this->validate($request, [
-            'nis' => 'required|integer|unique:App\Siswa,nis',
-            'nama' => 'required',
-            'jenis_kelamin' => [
-                'required',
-                Rule::in(['L', 'P']),
-            ],
-            'jurusan' => [
-                'required',
-                Rule::in(['AK', 'RPL', 'TKJ']),
-            ],
-            'kelas' => 'required|integer|gte:1|lte:6',
-            'tahun' => "required|integer|gte:2020|lte:{$tahun}",
-        ]);
-    
-        $affected = Siswa::insert([
-            'nis' => $request->nis,
-            'nama' => $request->nama,
-            'jenis_kelamin' => $request->jenis_kelamin,
-            'jurusan' => $request->jurusan,
-            'kelas' => $request->kelas,
-            'tahun' => $request->tahun,
-        ]);
-
-        if ($affected) {
-            return back()->with('status', ['success', 'Sukses tambah siswa']);
-        }
     }
 }
--- public/js/siswaform.js
+++ public/js/siswaform.js
@@ -3,27 +3,26 @@
  * @returns void
  */
 $(document).ready(function() {
-	$('#jurusan').change(function () {
-		var akhir = 0;
-
-		switch($('#jurusan').val()) {
-			case 'AK':
-				akhir = 5;
-				break;
-			case 'RPL':
-				akhir = 1;
-				break;
-			case 'TKJ':
-				akhir = 2;
-				break;
-		}
-
-		$('#kelas').children().each((i, pilihan) => {
-			if (i <= akhir) {
-				$(pilihan).prop('hidden', false);
-			} else {
-				$(pilihan).prop('hidden', true);
-			}
-		});
-	});
+	// Cek jurusan setelah form tampil
+	cekJurusan()
+	// Cek jurusan ketika jurusan terganti
+	$('#jurusan').change(cekJurusan());
 });
+
+function cekJurusan() {
+	var akhir = 0;
+
+	switch($('#jurusan').val()) {
+		case 'AK': akhir = 5;	break;
+		case 'RPL': akhir = 1; break;
+		case 'TKJ': akhir = 2; break;
+	}
+
+	$('#kelas').children().each((i, pilihan) => {
+		if (i <= akhir) {
+			$(pilihan).prop('hidden', false);
+		} else {
+			$(pilihan).prop('hidden', true);
+		}
+	}
+}

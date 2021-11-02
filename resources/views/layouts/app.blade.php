0cde6f0e5e1785f4a9d2aa6fc306340e8886ec9d
sglkc <31957516+sglkc@users.noreply.github.com>
2021-11-02 15:20:06 +0700

    Tambah sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,94 +13,87 @@
     <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
     <script src="{{ asset('js/popper.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/sidebar.js') }}"></script>
     @stack('script')
 
     <!-- Fonts -->
     <link href="//fonts.gstatic.com" rel="stylesheet">
     <link href="https://fonts.googleapis.com" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- Styles -->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('css/lightmode.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet">
     @stack('style')
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('pemetaan') }}">Hasil Pemetaan</a>
+        <nav id="sidebar">
+			<div class="custom-menu">
+				<button type="button" id="sidebarCollapse" class="btn btn-primary">
+					<i class="fa fa-bars"></i>
+				</button>
+			</div>
+			<div class="p-4 pt-5">
+                <h2>
+                    <img src="{{ asset('img/smkn13bandung.png') }}" alt="Logo SMKN 13 Bandung" width="40" height="40">
+                    <a class="text-white" href="{{ route('home') }}"><b>HUBIN-13</b></a>
+                </h2>
+                <ul class="list-unstyled mt-3">
+					<li class="active">
+						<a href="{{ route('home') }}"><i class="fa fa-home"></i>Home</a>
+					</li>
+					<li>
+                        <a href="#"><i class="fa fa-eye"></i>Lihat Data Pemetaan</a>
+					</li>
+					<li>
+                        <a href="{{ route('pemetaan') }}"><i class="fa fa-check"></i>Hasil Data Pemetaan</a>
+					</li>
+                    @if(Auth::getDefaultDriver() === 'admin')
+                        <li>
+                            <a href="{{ route('admin.siswa.data') }}"><i class="fa fa-users"></i>Data Siswa</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('tentang') }}">Tentang</a>
+                        <li>
+                            <a href="{{ route('admin.industri.data') }}"><i class="fa fa-industry"></i>Data Industri</a>
                         </li>
-                        <!-- Authentication Links -->
-                        @if(Auth::getDefaultDriver())
-                            @if(Auth::guard('admin')->check())
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('admin.home') }}">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('admin.industri.data') }}">Data Industri</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('admin.siswa.data') }}">Data Siswa</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('admin.pengaturan') }}">Pengaturan Akun</a>
-                                </li>
-                            @endif
-                            @if(Auth::guard('siswa')->check())
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('siswa.home') }}">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('siswa.pengaturan') }}">Pengaturan Akun</a>
-                                </li>
-                            @endif
-
-                            <span class="navbar-text">
-                                @if(Auth::guard('admin')->check())
-                                    {{ Auth::guard('admin')->user()->username }}
-                                @endif
-                                @if(Auth::guard('siswa')->check())
-                                    {{ Auth::guard('siswa')->user()->nama }}
-                                    {{ Auth::guard('siswa')->user()->nis }}
-                                @endif
-                            </span>
-
-                            @auth
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route(Auth::getDefaultDriver() . ".logout") }}">Logout</a>
-                                </li>
-                            @endauth
-                        @else
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('siswa.login') }}">Login</a>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
+                    @elseif(Auth::getDefaultDriver() === 'siswa')
+                        <li>
+                            <a href="{{ route('siswa.ajukan') }}"><i class="fa fa-paper-plane"></i>Ajukan Industri</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('siswa.pilih') }}"><i class="fa fa-list"></i>Pilih Industri</a>
+                        </li>
+                    @endif
+					<li>
+						<hr class="hr">
+					</li>
+                    @auth
+                        <li>
+                            <a href="{{ route(Auth::getDefaultDriver() . '.pengaturan') }}"><i class="fa fa-cogs"></i>Pengaturan</a>
+                        </li>
+                    @endauth
+					<li>
+						<a href="{{ route('tentang') }}"><i class="fa fa-comments"></i>Tentang</a>
+					</li>
+				</ul>
+			</div>
+			<div class="p-3 footer-gan">
+                <img src="{{ asset('img/smkn13bandung.png') }}" alt="logo smkn 13 bandung" class="rounded-circle border" width="50" height="50">
+				<div class="ml-3 my-auto flex-grow-1">
+                    <div>{{ Auth::user()->nama ?? Auth::user()->username ?? 'Belum Masuk' }}</div>
+                    <div>{{ Auth::user()->nis ?? '' }}</div>
+				</div>
+                @auth
+                    <a class="m-auto ml-3" href="{{ route(Auth::getDefaultDriver() . ".logout") }}">
+                        <i class="fa fa-sign-out fa-lg text-white"></i>
+                    </a>
+                @endauth
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="py-5">
             @yield('content')
         </main>
     </div>

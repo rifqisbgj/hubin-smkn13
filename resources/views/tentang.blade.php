e7b4920a3e2f4b5313bc2552f5b864f1dae7a977
sglkc <31957516+sglkc@users.noreply.github.com>
2021-12-03 15:47:52 +0700

    Tambah foto developer di halaman tentang

--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -20,7 +20,7 @@
         <div class="row row-cols-1 row-cols-sm-3">
             <div class="col mb-4">
                 <div class="card border-0">
-                    <img src="https://i.pinimg.com/564x/18/23/a6/1823a6eab786bc5112fff2b1a24edcc3.jpg" class="card-img-top" alt="Muhammad Irsyaad Fatahillah">
+                    <img src="{{ asset('img/tentang/irsyaad.png') }}" class="card-img-top" alt="Muhammad Irsyaad Fatahillah">
                     <div class="card-body">
                         <h4 class="card-text">Muhammad Irsyaad Fatahillah</h4>
                         <p class="card-text">Leader | Web Designer | <br> Front-End Developer</p>
@@ -34,7 +34,7 @@
 
             <div class="col mb-4">
                 <div class="card border-0">
-                    <img src="https://i.pinimg.com/564x/e9/bf/72/e9bf720d1b7065672deb8842095ff71d.jpg" class="card-img-top" alt="Fadhil Maulana">
+                    <img src="{{ asset('img/tentang/fadhil.png') }}" class="card-img-top" alt="Fadhil Maulana">
                     <div class="card-body">
                         <h4 class="card-text">Fadhil Maulana</h4>
                         <p class="card-text">Quality Assurance</p>
@@ -48,7 +48,7 @@
 
             <div class="col mb-4">
                 <div class="card border-0">
-                    <img src="https://i.pinimg.com/564x/04/1f/77/041f771a9d84af146f191d3ed61cd349.jpg" class="card-img-top" alt="Rifki Ahmad Maulana">
+                    <img src="{{ asset('img/tentang/Rifki.png') }}" class="card-img-top" alt="Rifki Ahmad Maulana">
                     <div class="card-body">
                         <h4 class="card-text">Rifki Ahmad Maulana</h4>
                         <p class="card-text">Web Designer | Front-End Developer</p>
@@ -62,7 +62,7 @@
 
             <div class="col mb-4">
                 <div class="card border-0">
-                    <img src="https://i.pinimg.com/564x/97/fb/89/97fb89e325460be9a8a38365cec92d51.jpg" class="card-img-top" alt="Sabrina Misyell Aaliyah">
+                    <img src="{{ asset('img/tentang/Sabrina.png') }}" class="card-img-top" alt="Sabrina Misyell Aaliyah">
                     <div class="card-body">
                         <h4 class="card-text">Sabrina Misyell Aaliyah</h4>
                         <p class="card-text">Web Designer | Front-End Developer</p>
@@ -76,7 +76,7 @@
 
             <div class="col mb-4">
                 <div class="card border-0">
-                    <img src="https://i.pinimg.com/564x/da/5f/14/da5f1439f1307906bcffef5017e2ac6e.jpg" class="card-img-top" alt="Cikal Gemintang Seya">
+                    <img src="{{ asset('img/tentang/Cikal.png') }}" class="card-img-top" alt="Cikal Gemintang Seya">
                     <div class="card-body">
                         <h4 class="card-text">Cikal Gemintang Seya</h4>
                         <p class="card-text">Back-End Developer</p>
@@ -90,7 +90,7 @@
 
             <div class="col mb-4">
                 <div class="card border-0">
-                    <img src="https://i.pinimg.com/564x/16/d3/94/16d3945fa89f2b98d60f744c8b34800a.jpg" class="card-img-top" alt="Rashad Razikin Zulkarnaen">
+                    <img src="{{ asset('img/tentang/Rashad.png') }}" class="card-img-top" alt="Rashad Razikin Zulkarnaen">
                     <div class="card-body">
                         <h4 class="card-text">Rashad Razikin Zulkarnaen</h4>
                         <p class="card-text">Back-End Developer</p>
